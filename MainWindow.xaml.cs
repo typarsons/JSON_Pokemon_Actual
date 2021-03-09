958854789c4089fee15fa62172ae361f00a66530
Tyler Parsons <tyjparsons@ou.edu>
2021-03-09 13:59:58 -0600

    finished in class review of this. prof solution is much cleaner, review for test

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace JSON_Pokemon_
     // this is correct file, build from here\\
     public partial class MainWindow : Window
     {
+        //in class 3/9 prof worked to a much cleaner solution
         public MainWindow()
         {
 
@@ -36,7 +37,8 @@ namespace JSON_Pokemon_
                 api = JsonConvert.DeserializeObject<allPokemonAPI>(json);
 
                 foreach (var resultObject in api.results.OrderBy(x => x.name).ToList())
-                {
+                  // can also do: foreach ( ResultObject item in api.results.OrderBy(x => x.name).ToList())
+                    {
 
                     lstPokemon.Items.Add(resultObject);
                 }//do this again 
@@ -47,8 +49,9 @@ namespace JSON_Pokemon_
             
             SelectedPokemo api;
             var selection = (ResultObject)lstPokemon.SelectedItem;
+            // can also do: ResultObject selection = (ResultObject)lstPokemon.SelectedItem;
             string urlSelected = selection.url;
-           // lbName.Content = selection.name;
+          
 
             var PokemonSelection = (ResultObject)lstPokemon.SelectedItem;
             using (var client = new HttpClient())
@@ -60,12 +63,48 @@ namespace JSON_Pokemon_
                 lbName.Content = api.name;
                 lbHeight.Content = api.height;
                 lbWeight.Content = api.weight;               
-               imgPokemon.Source = new BitmapImage(new Uri(api.sprites.back_default));
+               imgPokemon.Source = new BitmapImage(new Uri(api.sprites.front_default));
+          
             //this will sometimes result in error as certain pokemon such as "mega" forms do not
             //contain a value for front/back_default 
+            //could make a Char editor method like in one of the hw/parts and implement but not this time
+            //prof showed how to add to new window in class 3/9
         }
 
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
 
+        }
+        private bool myBool;
+        private void btnSwitch_Click(object sender, RoutedEventArgs e)
+        {
+            SelectedPokemo api;
+            var selection = (ResultObject)lstPokemon.SelectedItem;
+            string urlSelected = selection.url;
+            // lbName.Content = selection.name;
+
+            var PokemonSelection = (ResultObject)lstPokemon.SelectedItem;
+            using (var client = new HttpClient())
+            {
+                string json = client.GetStringAsync(urlSelected).Result;
+                api = JsonConvert.DeserializeObject<SelectedPokemo>(json);
+            }
+            if (myBool)
+            {
+                imgPokemon.Source = new BitmapImage(new Uri(api.sprites.front_default));
+            }
+            else
+            {
+                imgPokemon.Source = new BitmapImage(new Uri(api.sprites.back_default));
+            }
+            myBool = !myBool;
+            //https://stackoverflow.com/questions/31634471/how-do-i-change-one-image-to-another-upon-clicking-on-a-button/31635693
+            // i was told to cite sources in my mis 3013, putting this here just in case
+            // this is where i found the idea for setting a bool and using bool = !bool
+            // make sure to review how prof did this proble. much cleaner
+        }
+       
     }
         }
     
+        
